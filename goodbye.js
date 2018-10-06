819ddb7ceecc4a96e246bdcfcc58cfaab24a1740
Britany Statt <bstatt2@gmail.com>
2018-10-06 08:46:24 -0700

    basic node app checkpoint complete

---
+++ goodbye.js
@@ -0,0 +1,7 @@
+exports.sayByeEnglish = function(){
+  console.log('Goodbye');
+}
+
+exports.sayByeSpanish = function(){
+  console.log('Adios');
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+const greetings = require('./greeting.js');
+const goodbyes = require('./goodbye.js');
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbyes.sayByeEnglish();
+
+goodbyes.sayByeSpanish();
